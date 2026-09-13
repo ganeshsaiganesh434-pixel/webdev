ff10092e141682caea4732af8f49724a750fca8f
saiganesh <ganeshsaiganesh434@gmail.com>
2026-09-13 21:34:20 +0530

    move all zeroes to end

---
+++ moveallzeroestoendbettrapp.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void pushallzeroestoend(vector<int>&arr){
+    int count=0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]!=0){
+            arr[count++]=arr[i];
+        }
+    }
+    while(count<arr.size()){
+        arr[count++]=0;
+    }
+}
+
+int  main(){
+    vector<int>arr={1,2,0,4,3,0,5,0};
+    pushallzeroestoend(arr);
+    for(int num:arr)
+    {
+        cout<<num<<" ";
+
+    }
+    return 0;
+
+}
